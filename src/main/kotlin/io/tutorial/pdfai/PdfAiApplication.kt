eb0f633e6f20cd9d3c278516f18e18666c9a7641
Sangmin Lee <nalsm98@naver.com>
2024-12-13 08:55:58 +0900

    chore: initialize project

---
+++ src/main/kotlin/io/tutorial/pdfai/PdfAiApplication.kt
@@ -0,0 +1,11 @@
+package io.tutorial.pdfai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdfAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PdfAiApplication>(*args)
+}
---
+++ src/test/kotlin/io/tutorial/pdfai/PdfAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.tutorial.pdfai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PdfAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
